f48790a3007a4e1213386ab707c3ec1bc1d7451d
Chris O'Sullivan <thechrisoshow@gmail.com>
2018-04-27 14:16:31 +0100

    Add test for setting iban to same as account number if blank

--- spec/bank_detail_spec.rb
+++ spec/bank_detail_spec.rb
@@ -4,10 +4,38 @@ describe ShieldPay::BankDetail do
 
   it 'returns true if updated successfully' do
     stubbed_params = {
-      "AccountHolderName" => "MR Bananas",
+      "AccountHolderName" => "Mr Bananas",
+      "BankAccountNumber" => "123456",
       "CustomerKey" => "XXX",
       "CurrencyCode" => "GBP",
-      "RoutingNumber" => "123456",
+      "RoutingNumber" => "303030",
+      "IBAN" => "771177",
+    }
+
+    stub_request = stub_post_request("/BankDetail/AddBankDetail",
+                                     stubbed_params,
+                                     "bank_detail/updated_successfully.json")
+
+    bank_detail_params = {
+      account_holder_name: "Mr Bananas",
+      bank_account_number: "123456",
+      customer_key: "XXX",
+      currency_code: "GBP",
+      routing_number: "303030",
+      iban: "771177"
+    }
+
+    result = ShieldPay::BankDetail.update(bank_detail_params)
+    expect(result).to be_truthy
+  end
+
+  it 'sets IBAN to the same as bank_account_number if blank' do
+    stubbed_params = {
+      "AccountHolderName" => "Mr Bananas",
+      "BankAccountNumber" => "123456",
+      "CustomerKey" => "XXX",
+      "CurrencyCode" => "GBP",
+      "RoutingNumber" => "303030",
       "IBAN" => "123456",
     }
 
@@ -16,11 +44,11 @@ describe ShieldPay::BankDetail do
                                      "bank_detail/updated_successfully.json")
 
     bank_detail_params = {
-      account_holder_name: "MR Bananas",
+      account_holder_name: "Mr Bananas",
+      bank_account_number: "123456",
       customer_key: "XXX",
       currency_code: "GBP",
-      routing_number: "123456",
-      iban: "123456"
+      routing_number: "303030",
     }
 
     result = ShieldPay::BankDetail.update(bank_detail_params)
