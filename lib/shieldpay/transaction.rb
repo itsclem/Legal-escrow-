3dbae631b13516867340f6654d565aae16ee5424
Chris O'Sullivan <thechrisoshow@gmail.com>
2018-08-24 13:12:32 -0500

    Add ability to change status

--- lib/shieldpay/transaction.rb
+++ lib/shieldpay/transaction.rb
@@ -15,5 +15,19 @@ module ShieldPay
       response = Request.new.post("/Transaction/GetPaymentStatus", input_params)
       response.dig("Data", "StatusId")
     end
+
+    # Webhook Params
+    # Parameter           Optional?  Description
+    # customer_id         no 	       The customer id of either the sender or the
+    #                                receiver of this transaction
+    # transaction_id      no         The transaction id to change status
+    # status_id           no         The status to change to
+    def self.change_status(input_params)
+      stringify_keys!(input_params)
+      input_params["transaction_status_id"] = input_params.delete("status_id")
+      response = Request.new.post("/Transaction/ChangePaymentStatus",
+                                  input_params)
+      response.dig("coreRes", "status") == 1
+    end
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -19,4 +19,23 @@ describe ShieldPay::Transaction do
       expect(status_id).to eq(11)
     end
   end
+
+  describe '#change_status' do
+    it 'changes the status of the transaction' do
+      stubbed_params = {
+        "CustomerId" => "yyy",
+        "TransactionId" => "xxx",
+        "TransactionStatusId" => 4
+      }
+
+      stub_request = stub_post_request("/Transaction/ChangePaymentStatus",
+                                       stubbed_params,
+                                       "transaction/change_status.json")
+      params = {
+        customer_id: "yyy", transaction_id: "xxx", status_id: 4
+      }
+      result = ShieldPay::Transaction.change_status(params)
+      expect(result).to be_truthy
+    end
+  end
 end
